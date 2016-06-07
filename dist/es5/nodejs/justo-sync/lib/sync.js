63b112b432ebddb58f06f5d941eb4ce9104e7751
Raúl G. González <raulggonzalez@justojs.org>
2016-06-07 11:11:58 +0200

    Fix #3

--- dist/es5/nodejs/justo-sync/lib/sync.js
+++ dist/es5/nodejs/justo-sync/lib/sync.js
@@ -7,6 +7,7 @@
 
 
 
+
 function (fn) {
   var result;
 
--- test/unit/lib/sync.js
+++ test/unit/lib/sync.js
@@ -10,47 +10,115 @@ const sync = require("../../../dist/es5/nodejs/justo-sync/lib/sync").default;
 
 //suite
 suite("sync", function() {
-  suite("Value to return", function() {
-    test("sync(async : function) - done()", function() {
-      assert(sync(function(done) {
-        process.nextTick(function() {
-          done();
-        });
-      }) === undefined);
+  suite("Not nested", function() {
+    suite("Value to return", function() {
+      test("sync(async : function) - done()", function() {
+        assert(sync(function(done) {
+          process.nextTick(function() {
+            done();
+          });
+        }) === undefined);
+      });
+
+      test("sync(async : function) : done(undefined, value)", function() {
+        sync(function(done) {
+          fs.readdir("test/unit/", function(error, files) {
+            done(undefined, "the value");
+          });
+        }).must.be.eq("the value");
+      });
+
+      test("sync(async : function) : done(null, value)", function() {
+        sync(function(done) {
+          fs.readdir("test/unit/", function(error, files) {
+            done(null, "the value");
+          });
+        }).must.be.eq("the value");
+      });
     });
 
-    test("sync(async : function) : done(undefined, value)", function() {
-      sync(function(done) {
-        fs.readdir("test/unit/", function(error, files) {
-          done(undefined, "the value");
-        });
-      }).must.be.eq("the value");
-    });
+    suite("Exception to throw", function() {
+      test("sync(async : function) : done(error : Error)", function() {
+        sync.must.raise(Error, [function(done) {
+          fs.readdir("unknown", function(error, files) {
+            done(error);
+          });
+        }]);
+      });
 
-    test("sync(async : function) : done(null, value)", function() {
-      sync(function(done) {
-        fs.readdir("test/unit/", function(error, files) {
-          done(null, "the value");
-        });
-      }).must.be.eq("the value");
+      test("sync(async : function) : done(error : string)", function() {
+        sync.must.raise(Error, [function(done) {
+          fs.readdir("unknown", function(error, files) {
+            done("an error");
+          });
+        }]);
+      });
     });
   });
 
-  suite("Exception to throw", function() {
-    test("sync(async : function) : done(error : Error)", function() {
-      sync.must.raise(Error, [function(done) {
-        fs.readdir("unknown", function(error, files) {
-          done(error);
-        });
-      }]);
+  suite("Nested", function() {
+    suite("Value to return", function() {
+      test("sync(async : function) - done()", function() {
+        assert(sync(function(done) {
+          process.nextTick(function() {
+            sync(function(done) {
+              done();
+            });
+
+            done();
+          });
+        }) === undefined);
+      });
+
+      test("sync(async : function) : done(undefined, value)", function() {
+        sync(function(done) {
+          var res = sync(function(done) {
+            fs.readdir("test/unit/", function(error, files) {
+              done(undefined, "the value");
+            });
+          });
+
+          done(undefined, res);
+        }).must.be.eq("the value");
+      });
+
+      test("sync(async : function) : done(null, value)", function() {
+        sync(function(done) {
+          var res = sync(function(done) {
+            fs.readdir("test/unit/", function(error, files) {
+              done(undefined, "the value");
+            });
+          });
+
+          done(null, res);
+        }).must.be.eq("the value");
+      });
     });
 
-    test("sync(async : function) : done(error : string)", function() {
-      sync.must.raise(Error, [function(done) {
-        fs.readdir("unknown", function(error, files) {
-          done("an error");
-        });
-      }]);
+    suite("Exception to throw", function() {
+      test("sync(async : function) : done(error : Error)", function() {
+        sync.must.raise(Error, [function(done) {
+          var error = sync(function(done) {
+            fs.readdir("unknown", function(error, files) {
+              done(undefined, error);
+            });
+          });
+
+          done(error);
+        }]);
+      });
+
+      test("sync(async : function) : done(error : string)", function() {
+        sync.must.raise(Error, [function(done) {
+          var error = sync(function(done) {
+            fs.readdir("unknown", function(error, files) {
+              done(undefined, "an error");
+            });
+          });
+
+          done(error);
+        }]);
+      });
     });
   });
 })();
